7badbd4d923e882d20a1515804a5ba898b62c496
WodaMineralna <raknieborak65@gmail.com>
2025-04-19 16:36:41 +0200

    validating input on every keystroke via state

--- src/components/useStateLogin.jsx
+++ src/components/useStateLogin.jsx
@@ -6,6 +6,9 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -33,6 +36,11 @@ export default function Login() {
             onChange={(event) => handleInputChange("email", event)}
             value={enteredValues.email}
           />
+          {emailIsInvalid && (
+            <div className="control-error">
+              <p>Please enter a valid email adress.</p>
+            </div>
+          )}
         </div>
 
         <div className="control no-margin">
